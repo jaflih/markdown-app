2cb31d8cd4124ce2bc6c95613a2b0bf5b5d5a0bc
jihaneh <jijih@hotmail.fr>
2022-10-25 10:09:56 +0200

    Add state

--- src/App.js
+++ src/App.js
@@ -4,12 +4,21 @@ import './App.css';
 import { sampleText } from './sampleText';
 
 class App extends Component {
+  state = {
+    text: sampleText,
+  };
+
+  handleChange = (event) => {
+    const text = event.target.value;
+    this.setState({ text });
+  };
+
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-sm-6">
-            <textarea rows="35" className="form-control" value={sampleText}></textarea>
+            <textarea rows="35" className="form-control" value={this.state.text} onChange={this.handleChange}></textarea>
           </div>
           <div className="col-sm-6">{sampleText}</div>
         </div>
