df0d9d0fc870546721efa5371a16606a67106ef0
jihaneh <jijih@hotmail.fr>
2022-10-25 09:56:56 +0200

    Add marked, Bootstrap  ans SampleText

---
+++ src/sampleText.js
@@ -0,0 +1,2 @@
+export const sampleText =
+  '# Titre\n## Sous-Titre \n### Titre plus profond\n \nLes paragraphes sont séparés\npar une ligne vide.\n\nLaissé deux espaces à la fin d\'une ligne pour  \naller à la ligne.\n\nAttributs *italique*, **gras**, \n`monospace`, ~~rayé~~.\n\nListe:\n\n  * pommes\n  * oranges\n  * poires\n\nListe numérotée:\n\n  1. dinde\n  2. cornichon\n  3. pain\n\n *[Me](https://www.youtube.com)* \n\n Lien automatique : https://www.facebook.com/ \n\n```\n console.log("hello"); \n``` ';
