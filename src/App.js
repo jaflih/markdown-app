e50c3c7eec2b6d06dcf02ba4a62c3a5a75caecd7
jihaneh <jijih@hotmail.fr>
2022-10-25 10:04:23 +0200

    Update App to React Component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
 import { sampleText } from './sampleText';
 
-function App() {
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-sm-6">
-          <textarea rows="35" className="form-control" value={sampleText}></textarea>
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6">
+            <textarea rows="35" className="form-control" value={sampleText}></textarea>
+          </div>
+          <div className="col-sm-6">{sampleText}</div>
         </div>
-        <div className="col-sm-6">{sampleText}</div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
